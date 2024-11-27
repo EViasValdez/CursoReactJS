f3c908fa2b42865457440f2122390b673dfa2548
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-11-26 22:03:33 -0600

    Add files via upload

---
+++ primer-proyecto/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome from "./componentes/Welcome.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Primer programa para probar las funciones de ReactJS
+        </p>
+        <Welcome Mensaje="Bienvenido props" Nombre="Eduardo"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
